84ec355a504c2d6a86a786af12e199a0bf809243
Hall Enoch Asanda <hallenochasanda@gmail.com>
2022-10-31 10:46:42 +0300

    feat: add rating model, serializer

---
+++ apps/ratings/serializers.py
@@ -0,0 +1,23 @@
+# serializers allow complex data such as query sets and model instances to be converted
+# to native python data types, these data types are easily rendered into json,xml content types
+
+from rest_framework import serializers
+from .models import Rating
+
+class RatingSerializer(serializers.ModelSerializer):
+    # method fields have corresponding methods as defined below
+    # get_rater
+    rater = serializers.SerializerMethodField(read_only=True)
+    # get_agent
+    agent = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Rating
+        exclude = ["updated_at","pkid"]
+
+    def get_rater(self,obj):
+        return obj.rater.username
+
+    def get_agent(self,obj):
+        # agent goes to profile model, in profile model the relationship btn profile and user model is the user 
+        return obj.agent.user.username
