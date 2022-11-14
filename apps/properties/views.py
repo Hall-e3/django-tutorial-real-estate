d11b26898b8331e289a356708d0621c90b2251e6
Hall Enoch Asanda <hallenochasanda@gmail.com>
2022-11-14 22:59:27 +0300

    feat:view

--- apps/properties/views.py
+++ apps/properties/views.py
@@ -17,4 +17,7 @@ from .serializers import (
 logger = logging.getLogger(__name__)
 
 class PropertyFilter(django_filters.FilterSet):
-    advert_type = django_filters.ChoiceFilter(field_name="advert_type",lookup_expr="iexact")
+    advert_type = django_filters.CharFilter(field_name="advert_type",lookup_expr="iexact")
+    property_type = django_filters.CharFilter(field_name="property_type",lookup_expr="iexact")
+    price = django_filters.NumberFilter()
+    price__gt = django_filters.NumberFilter(field_name="")
