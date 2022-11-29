a38b71746443a5da290eb50b2bf5fb7147532dd9
Hall Enoch Asanda <hallenochasanda@gmail.com>
2022-11-29 14:49:17 +0300

    new:features

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -11,8 +11,8 @@ class PropertySerializer(serializers.ModelSerializer):
         model = Property
         fields = ["id","user","title","slug","ref_code","description","country","city","postal_code","street_address","property_number","price","tax","final_property_price","plot_area","total_floors","bedrooms","bathrooms","advert_type","property_type","cover_photo","photo1","photo2","photo3","photo4","published_status","views"]
 
-        def get_user(self,obj):
-            return obj.user.username
+    def get_user(self,obj):
+        return obj.user.username
 
 
 class PropertyCreateSerializer(serializers.ModelSerializer):
@@ -20,10 +20,10 @@ class PropertyCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Property
-        exclude = ["update_at","pkid"]
+        exclude = ["updated_at","pkid"]
 
 
 class PropertyViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = PropertyViews
-        exclude = ["update_at","pkid"]
+        exclude = ["updated_at","pkid"]
