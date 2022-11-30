93ba3f13f3a399535ff22f85758c0b0369230c12
Hall Enoch Asanda <hallenochasanda@gmail.com>
2022-11-30 10:59:07 +0300

    docker-compose

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -3,13 +3,22 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path,include
 
+# we are serving things locally
+# urlpatterns = [
+#     path('supersecret/', admin.site.urls),
+#     path("api/v1/auth/",include("djoser.urls")),
+#     path("api/v1/auth/",include("djoser.urls.jwt")),
+#     path("api/v1/profile/",include("apps.profiles.urls")),
+#     path("api/v1/properties/",include("apps.properties.urls")),
+# ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     path('supersecret/', admin.site.urls),
     path("api/v1/auth/",include("djoser.urls")),
     path("api/v1/auth/",include("djoser.urls.jwt")),
     path("api/v1/profile/",include("apps.profiles.urls")),
     path("api/v1/properties/",include("apps.properties.urls")),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] 
 
 admin.site.site_header = "Real Estate Admin"
 admin.site.site_title = "Real Estate Admin Portal"
