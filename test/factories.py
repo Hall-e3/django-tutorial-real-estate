0b5f0393814170728bd474933b59bef2d63f174e
Hall Enoch Asanda <hallenochasanda@gmail.com>
2022-12-02 11:40:56 +0300

    test file complete

--- test/factories.py
+++ test/factories.py
@@ -49,3 +49,7 @@ class UserFactory(factory.django.DjangoModelFactory):
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
         manager = cls._get_manager(model_class)
+        if "is_superuser" in kwargs:
+            return manager.create_superuser(*args,**kwargs)
+        else:
+            return manager.create_user(*args,**kwargs)
